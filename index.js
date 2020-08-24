48da5153e5ff1e662724d118ab771f63b7944f35
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2020-08-24 19:51:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const titleCased = () => {
 };
 function capitalize_Words(str) {
   return str.replace(/\w\S*/g, function (txt) {
-    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    return txt.charAt(0).toUpperCase() + txt.substr(1);
   });
 }
+
